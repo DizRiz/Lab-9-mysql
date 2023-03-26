09d1aadb2501f42098b192b3fcb969e67e7ff23c
DizRiz <mdfarishakim@gmail.com>
2023-03-26 15:38:31 +0800

    Adds Comment functionality

---
+++ models/comment.js
@@ -0,0 +1,30 @@
+const { DataTypes, Model } = require("sequelize");
+let dbConnect = require("../dbConnect");
+const User = require('./user');
+const Post = require('./post');
+
+const sequelizeInstance  = dbConnect.Sequelize;
+
+class comment extends Model {}
+
+comment.init({
+     comment: { type: DataTypes.STRING, allowNull: false, required: true },
+     postId: {
+       type: DataTypes.INTEGER,
+       references: {
+         model: Post,
+         key: 'id'
+       }
+     },
+     userId: {
+       type: DataTypes.INTEGER,
+       references: {
+         model: User,
+         key: 'id'
+       }
+     }
+}, {sequelize: sequelizeInstance, modelName: 'comment', timestamps: true, freezeTableName: true})
+
+comment.belongsTo(User);
+comment.belongsTo(Post);
+module.exports = comment;
